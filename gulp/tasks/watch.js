4932cde89e89962f58de4d1ce021721a55b574a0
anatolikurtsevich <anatoly.kurtsevich@itechart-group.com>
2014-07-14 15:25:25 +0300

    updated watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,9 +3,9 @@
 var gulp = require('gulp');
 
 
-gulp.task('watch', ['serve'], function () {
+gulp.task('watch', ['serve:dev'], function () {
     gulp.watch('src/less/**', ['less']);
     gulp.watch('src/images/**', ['images']);
     gulp.watch('src/html/index.html', ['copy']);
-    gulp.watch(['src/html/index.html', 'src/js/**/*.js'], ['dev', 'scripts']);
+    gulp.watch(['src/html/index.html', 'src/js/**/*.js'], ['env:dev', 'scripts']);
 });
